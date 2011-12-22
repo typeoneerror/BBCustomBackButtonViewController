c9e9e4b773f71a0a8aef825c44d973584bfd4d28
Ben Borowski <ben.borowski@typeoneerror.com>
2011-12-21 16:13:02 -0800

    Initial Commit

---
+++ BBCustomBackButtonViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BBCustomBackButtonViewController
+//
+//  Created by Benjamin Borowski on 12/21/11.
+//  Copyright (c) 2011 Typeoneerror Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BBCustomBackButtonViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BBCustomBackButtonViewController
+//
+//  Created by Benjamin Borowski on 12/21/11.
+//  Copyright (c) 2011 Typeoneerror Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
